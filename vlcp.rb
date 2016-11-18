1507d490146821a32fde77796e0c5ecdc990e460
chikamim <chikami@exdit.com>
2016-11-18 22:56:33 +0900

    brew cask

---
+++ vlcp.rb
@@ -0,0 +1,9 @@
+class Vlcp < Cask
+  version '1.1.11'
+  sha256 '38ec0f43d0ac799fa4e599d5d6f7e2efef8b71bd67dd0fa51e3f032eb2ddc276'
+
+  url 'https://github.com/chikamim/homebrew-vlcp/releases/download/1.1.13p/vlcp.zip'
+  homepage ''
+
+  link 'VLCp.app'
+end
